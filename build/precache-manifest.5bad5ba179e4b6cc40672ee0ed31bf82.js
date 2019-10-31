5990ace4153bec793de16f01582ac1dbae970486
Maliha Tanweer <malihatanweer@gmail.com>
2019-10-31 18:05:08 -0400

    Start with create-react-app

---
+++ build/precache-manifest.5bad5ba179e4b6cc40672ee0ed31bf82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9403bd5f59a3fde30a4a",
+    "url": "/malihat/techdegree-project-7/static/js/runtime~main.a4c5d892.js"
+  },
+  {
+    "revision": "854c1b722806a538d75f",
+    "url": "/malihat/techdegree-project-7/static/js/main.4792cfa3.chunk.js"
+  },
+  {
+    "revision": "32b5f1ccd49924c0e9c3",
+    "url": "/malihat/techdegree-project-7/static/js/2.517dedda.chunk.js"
+  },
+  {
+    "revision": "854c1b722806a538d75f",
+    "url": "/malihat/techdegree-project-7/static/css/main.68b5137f.chunk.css"
+  },
+  {
+    "revision": "b855c8f1ad14d14128ac588366f9fa58",
+    "url": "/malihat/techdegree-project-7/index.html"
+  }
+];
